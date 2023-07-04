8b0a60b97b696f9d6497562137ac09a144161cb4
walterccrs <78065039+walterccrs@users.noreply.github.com>
2023-07-03 22:17:47 -0300

    Add files via upload

---
+++ 1/04/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*EJERCICIO 1-4*/
+
+/*print Fahrenheit-Celsius table
+for fahr = 0, 20, ..., 300 */
+
+main()
+{
+    float cel;
+    int fahr, lower, upper, step;
+
+    lower = -17.8;      /* lower limit of temperature table*/
+    upper = 148.9;    /* upper limit */
+    step = 10;      /* step size */
+
+    printf("\tCelsius\t\tFahrenheit\n");
+    cel = lower;
+    while (cel <= upper) {
+        fahr = (cel*9.0/5.0) + 32.0;
+        printf("\t   %5.1f\t%6d\n",cel,fahr);
+        cel = cel + step;
+    }
+}
